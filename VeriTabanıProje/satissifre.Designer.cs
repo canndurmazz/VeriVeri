1e66a63b10c4b41c44abba9b460fbf34c7f2d412
canndurmazz <81639795+canndurmazz@users.noreply.github.com>
2021-04-30 16:30:24 +0300

    0

--- VeriTabanıProje/satissifre.Designer.cs
+++ VeriTabanıProje/satissifre.Designer.cs
@@ -31,8 +31,8 @@ namespace VeriTabanıProje
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(satissifre));
             this.button1 = new System.Windows.Forms.Button();
-            this.kullaniciSifre = new System.Windows.Forms.TextBox();
-            this.kullaniciAd = new System.Windows.Forms.TextBox();
+            this.kullaici_sifre = new System.Windows.Forms.TextBox();
+            this.kullanici_ad = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -52,27 +52,27 @@ namespace VeriTabanıProje
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // kullaniciSifre
+            // kullaici_sifre
             // 
-            this.kullaniciSifre.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.kullaniciSifre.Location = new System.Drawing.Point(76, 51);
-            this.kullaniciSifre.Name = "kullaniciSifre";
-            this.kullaniciSifre.Size = new System.Drawing.Size(146, 22);
-            this.kullaniciSifre.TabIndex = 15;
+            this.kullaici_sifre.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.kullaici_sifre.Location = new System.Drawing.Point(76, 59);
+            this.kullaici_sifre.Name = "kullaici_sifre";
+            this.kullaici_sifre.Size = new System.Drawing.Size(146, 22);
+            this.kullaici_sifre.TabIndex = 15;
             // 
-            // kullaniciAd
+            // kullanici_ad
             // 
-            this.kullaniciAd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.kullaniciAd.Location = new System.Drawing.Point(76, 17);
-            this.kullaniciAd.Name = "kullaniciAd";
-            this.kullaniciAd.Size = new System.Drawing.Size(146, 22);
-            this.kullaniciAd.TabIndex = 14;
+            this.kullanici_ad.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.kullanici_ad.Location = new System.Drawing.Point(76, 25);
+            this.kullanici_ad.Name = "kullanici_ad";
+            this.kullanici_ad.Size = new System.Drawing.Size(146, 22);
+            this.kullanici_ad.TabIndex = 14;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(0, 54);
+            this.label2.Location = new System.Drawing.Point(0, 62);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 17);
             this.label2.TabIndex = 13;
@@ -82,7 +82,7 @@ namespace VeriTabanıProje
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Location = new System.Drawing.Point(16, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 17);
             this.label1.TabIndex = 12;
@@ -91,10 +91,11 @@ namespace VeriTabanıProje
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(205, -3);
+            this.label3.Location = new System.Drawing.Point(202, 5);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.Size = new System.Drawing.Size(20, 20);
             this.label3.TabIndex = 17;
             this.label3.Text = "X";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -102,10 +103,11 @@ namespace VeriTabanıProje
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(183, -3);
+            this.label4.Location = new System.Drawing.Point(180, 5);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.Size = new System.Drawing.Size(19, 20);
             this.label4.TabIndex = 18;
             this.label4.Text = "<";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -115,7 +117,7 @@ namespace VeriTabanıProje
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(3, 75);
+            this.button2.Location = new System.Drawing.Point(3, 85);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(49, 50);
             this.button2.TabIndex = 19;
@@ -127,13 +129,13 @@ namespace VeriTabanıProje
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(231, 134);
+            this.ClientSize = new System.Drawing.Size(226, 134);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.kullaniciSifre);
-            this.Controls.Add(this.kullaniciAd);
+            this.Controls.Add(this.kullaici_sifre);
+            this.Controls.Add(this.kullanici_ad);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -146,8 +148,8 @@ namespace VeriTabanıProje
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox kullaniciSifre;
-        private System.Windows.Forms.TextBox kullaniciAd;
+        private System.Windows.Forms.TextBox kullaici_sifre;
+        private System.Windows.Forms.TextBox kullanici_ad;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
