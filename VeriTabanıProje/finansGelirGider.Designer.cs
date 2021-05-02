582c2e9b2639f05450b242ffa26df7800b2874e1
zzidliynanekk@gmail.com <zzidliynanekk@gmail.com>
2021-05-03 00:08:36 +0300

    ..

---
+++ VeriTabanıProje/finansGelirGider.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace VeriTabanıProje
+{
+    partial class finansGelirGider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textMaasgider = new System.Windows.Forms.TextBox();
+            this.textAlisGider = new System.Windows.Forms.TextBox();
+            this.textSatisgelir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textToplam = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textMaasgider
+            // 
+            this.textMaasgider.BackColor = System.Drawing.SystemColors.Window;
+            this.textMaasgider.Enabled = false;
+            this.textMaasgider.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textMaasgider.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textMaasgider.Location = new System.Drawing.Point(136, 29);
+            this.textMaasgider.Name = "textMaasgider";
+            this.textMaasgider.Size = new System.Drawing.Size(100, 20);
+            this.textMaasgider.TabIndex = 0;
+            // 
+            // textAlisGider
+            // 
+            this.textAlisGider.BackColor = System.Drawing.SystemColors.Window;
+            this.textAlisGider.Enabled = false;
+            this.textAlisGider.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textAlisGider.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textAlisGider.Location = new System.Drawing.Point(136, 75);
+            this.textAlisGider.Name = "textAlisGider";
+            this.textAlisGider.Size = new System.Drawing.Size(100, 20);
+            this.textAlisGider.TabIndex = 1;
+            // 
+            // textSatisgelir
+            // 
+            this.textSatisgelir.BackColor = System.Drawing.SystemColors.Window;
+            this.textSatisgelir.Enabled = false;
+            this.textSatisgelir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textSatisgelir.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textSatisgelir.Location = new System.Drawing.Point(136, 125);
+            this.textSatisgelir.Name = "textSatisgelir";
+            this.textSatisgelir.Size = new System.Drawing.Size(100, 20);
+            this.textSatisgelir.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(66, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Maaş Gider:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(76, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Alış Gider:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(76, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Satış Gelir";
+            // 
+            // textToplam
+            // 
+            this.textToplam.BackColor = System.Drawing.SystemColors.Window;
+            this.textToplam.Enabled = false;
+            this.textToplam.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textToplam.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textToplam.Location = new System.Drawing.Point(136, 166);
+            this.textToplam.Name = "textToplam";
+            this.textToplam.Size = new System.Drawing.Size(100, 20);
+            this.textToplam.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(88, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Toplam";
+            // 
+            // labelDurum
+            // 
+            this.labelDurum.AutoSize = true;
+            this.labelDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelDurum.Location = new System.Drawing.Point(132, 200);
+            this.labelDurum.Name = "labelDurum";
+            this.labelDurum.Size = new System.Drawing.Size(0, 20);
+            this.labelDurum.TabIndex = 7;
+            // 
+            // finansGelirGider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(327, 242);
+            this.Controls.Add(this.textToplam);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelDurum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textSatisgelir);
+            this.Controls.Add(this.textAlisGider);
+            this.Controls.Add(this.textMaasgider);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "finansGelirGider";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "finansGelirGider";
+            this.Load += new System.EventHandler(this.finansGelirGider_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textMaasgider;
+        private System.Windows.Forms.TextBox textAlisGider;
+        private System.Windows.Forms.TextBox textSatisgelir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textToplam;
+        private System.Windows.Forms.Label labelDurum;
+    }
+}
---
+++ VeriTabanıProje/finansGelirGider.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace VeriTabanıProje
+{
+    public partial class finansGelirGider : Form
+    {
+
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        public finansGelirGider()
+        {
+            InitializeComponent();
+        }
+
+        private void finansGelirGider_Load(object sender, EventArgs e)
+        {
+            double a, b, c;
+            try
+            {
+                baglanti = new SqlConnection("server =USER11\\SQLEXPRESS; Initial Catalog = fabrikavt; Integrated Security = SSPI");
+                baglanti.Open();
+
+                string sorgu = "Select sum(personel_maas) from personel";
+                SqlCommand komut = new SqlCommand(sorgu, baglanti);
+                if (komut.ExecuteScalar().ToString() == "")
+                {
+                    a = 0;
+                    textMaasgider.Text = a.ToString();
+                }
+                else
+                {
+                    a = Convert.ToDouble(komut.ExecuteScalar());
+                    textMaasgider.Text = a.ToString();
+                }
+                
+                string sorgu1 = "Select sum(alim_fiyat) from alım";
+                SqlCommand komut1 = new SqlCommand(sorgu1, baglanti);
+                if(komut1.ExecuteScalar().ToString()=="")
+                {
+                    b = 0;
+                    textAlisGider.Text = b.ToString();
+                }
+                else
+                {
+                    b = Convert.ToDouble(komut1.ExecuteScalar());
+                    textAlisGider.Text = b.ToString();
+                }
+                
+                string sorgu2 = "Select sum(satıs_fiyat) from satis";
+                SqlCommand komut2 = new SqlCommand(sorgu2, baglanti);
+                if (komut2.ExecuteScalar().ToString() == "")
+                {
+                    c = 0;
+                    textSatisgelir.Text = c.ToString();
+                }
+                else
+                {
+                    c = Convert.ToDouble(komut2.ExecuteScalar());
+                    textSatisgelir.Text = c.ToString();
+                }
+                
+                baglanti.Close();
+
+                double sonuc = -a - b + c;
+                textToplam.Text = sonuc.ToString();
+                if (sonuc>0)
+                {
+                    labelDurum.Text = "Kar Sağlandı";
+                    labelDurum.ForeColor = Color.Green;
+                }
+                else if (sonuc==0)
+                {
+                    labelDurum.Text = "Gelir-Gider Eşit";
+                    labelDurum.ForeColor = Color.White;
+                }
+                else
+                {
+                    labelDurum.Text = "ZARAR";
+                    labelDurum.ForeColor = Color.Red;
+                }
+            }
+            catch(Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+
+
+            
+        }
+    }
+}
